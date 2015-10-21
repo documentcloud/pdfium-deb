7b1306280961c1fd5150673a9bb237f324bf6b6c
Ted Han <ted@knowtheory.net>
2015-10-21 15:46:04 -0700

    cleanup, simplify and better annotate build script runner.

--- BUILD.sh
+++ BUILD.sh
@@ -5,10 +5,10 @@ set -e
 # Today's date is...
 DATE=`date +'%Y%m%d'`
 VERSION=0.1
-NAME=pdfium-$VERSION+git$DATE
+NAME=pdfium-$1
 
 # Check whether the change log has been updated today.
-CHANGELOG_VERSION=`head -1 debian-config/changelog|perl -ne 'print $1 if /pdfium\s\((.*?)\-\d+\)/'`
+CHANGELOG_VERSION=`head -1 debian-config/changelog|perl -ne 'print $1 if /pdfium\s\(((.*?)[.-]\d+)\)/'`
 if [ "$NAME" != "pdfium-$CHANGELOG_VERSION" ]; then
   # exit if it hasn't.
   echo Attempting to build version from $NAME, but changelog has pdfium-$CHANGELOG_VERSION
@@ -16,53 +16,38 @@ if [ "$NAME" != "pdfium-$CHANGELOG_VERSION" ]; then
 fi
 
 echo "Fetching PDFium source"
-if [ -e "$1" ]
+if [ -e "$2" ]
 then
-  cp -r $1 $NAME
-  cd $NAME
+  echo "Copying source from $2"
+  cp -r $2 $NAME
+else
+  REPO=https://pdfium.googlesource.com/pdfium.git
+  echo "Cloning source from $REPO"
+  git clone $REPO $NAME
+fi
+
+cd $NAME # drop down into pdfium source directory
+
+echo "Reset repo and pull most recent updates"
+#git reset --hard
+git checkout master
+git pull
+
+if [ -e ./build/gyp ]; then
+  echo "gyp tools exist, pulling"
+  cd ./build/gyp
   git reset --hard
   git checkout master
   git pull
-
-  if [ -e ./build/gyp ]; then
-    cd ./build/gyp
-    git reset --hard
-    git checkout master
-    git pull
-    cd ../..
-  else
-    git clone https://chromium.googlesource.com/external/gyp build/gyp
-  fi
-  
-  if [ -e v8 ]; then
-    cd v8
-    git reset --hard
-    git checkout master
-    git pull
-    cd ..
-  else
-    git clone https://chromium.googlesource.com/v8/v8.git v8
-  fi
-  
-  if [ -e v8/third_party/icu ]; then
-    cd v8/third_party/icu
-    git reset --hard
-    git checkout master
-    git pull
-    cd ../../..
-  else
-    git clone https://chromium.googlesource.com/chromium/deps/icu46 v8/third_party/icu
-  fi
-  cd ..
+  cd ../..
 else
-  git clone https://pdfium.googlesource.com/pdfium.git $NAME
-  cd $NAME
+  echo "Fetching gyp tools"
   git clone https://chromium.googlesource.com/external/gyp build/gyp
-  git clone https://chromium.googlesource.com/v8/v8.git v8
-  git clone https://chromium.googlesource.com/chromium/deps/icu46 v8/third_party/icu
-  cd ..
 fi
 
+cd .. # pop up out of pdfium source directory.
+
+echo $(pwd)
 echo "Zipping PDFium source"
 tar czf $NAME.orig.tar.gz $NAME
 
@@ -75,11 +60,11 @@ echo As the info they collect will be overwritten with information from the debi
 dh_make -f ../$NAME.orig.tar.gz
 
 # copy customized deb config files
-cp -r ../debian-config/rules ./debian/
-cp -r ../debian-config/control ./debian/
+cp -r ../debian-config/rules                 ./debian/
+cp -r ../debian-config/control               ./debian/
 cp -r ../debian-config/libpdfium-dev.install ./debian/
-cp -r ../debian-config/patches  ./debian/
-cp -r ../debian-config/changelog  ./debian/
+cp -r ../debian-config/patches               ./debian/
+cp -r ../debian-config/changelog             ./debian/
 
 # remove "-us -uc" flags to build signed deb.  Must have trusted gpg key
 dpkg-buildpackage -us -uc
